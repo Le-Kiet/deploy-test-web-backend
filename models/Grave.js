601ec46e7eba937d819ec56922c67b8e405429a6
lekiet <116272203+Le-Kiet@users.noreply.github.com>
2025-06-02 20:41:02 +0700

    Update Grave.js

--- models/Grave.js
+++ models/Grave.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const GraveSchema = new mongoose.Schema({
-  grave: { type: [String], required: true },
+  grave: { type: String, required: true },
   generation: { type: Number },
-  location: { type: [String], required: true },
-  note: { type: [String] },
+  location: { type: String, required: true },
+  note: { type: String },
   geom: {
     type: { type: String, enum: ['Point'], required: true },
     coordinates: { type: [Number], required: true } // [lng, lat]
